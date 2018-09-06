52abb60b785fa241a90dd5c179ff984a9e505d95
Grigory Zamkovets <Green5050@mail.ru>
2018-09-06 17:25:50 +0300

    Second Commit

--- bootstrapsite1/js/javascript.js
+++ bootstrapsite1/js/javascript.js
@@ -3,3 +3,14 @@ $('.carousel').carousel({
 	interval: 5000,
 	keyboard: false
 })
+
+// отмена escape для закрытия модального окна
+$('#exampleModal1').modal ({
+	keyboard: false
+})
+$('#exampleModal2').modal ({
+    keyboard: false
+})
+$('#exampleModal3').modal ({
+    keyboard: false
+})
